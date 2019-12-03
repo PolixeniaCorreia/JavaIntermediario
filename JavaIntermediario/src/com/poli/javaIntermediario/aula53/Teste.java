4cf745b324fd65c1eae5b420a8144c834f3aafe0
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-12-03 16:29:53 -0300

    Usando Constantes

---
+++ JavaIntermediario/src/com/poli/javaIntermediario/aula53/Teste.java
@@ -0,0 +1,67 @@
+package com.poli.javaIntermediario.aula53;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		usandoConstantes();
+
+	}
+
+	private static void usandoConstantes() {
+
+		int segunda = DiaSemanaConstantes.SEGUNDA;
+		int terca = DiaSemanaConstantes.TERCA;
+		int quarta = DiaSemanaConstantes.QUARTA;
+		int quinta = DiaSemanaConstantes.QUINTA;
+		int sexta = DiaSemanaConstantes.SEXTA;
+		int sabado = DiaSemanaConstantes.SABADO;
+		int domingo = DiaSemanaConstantes.DOMINGO;
+
+
+		System.out.println("Teste utilizando constantes no Java");
+
+		imprimeDiaSemana(segunda);	
+		imprimeDiaSemana(terca);
+		imprimeDiaSemana(quarta);
+		imprimeDiaSemana(quinta);
+		imprimeDiaSemana(sexta);
+		imprimeDiaSemana(sabado);
+		imprimeDiaSemana(domingo);
+
+	}
+
+	private static void imprimeDiaSemana(int dia) {
+
+		switch (dia) {
+		case 1:
+			System.out.println("Segunda-Feira");
+			break;
+		case 2:
+			System.out.println("Terça-Feira");
+			break;
+		case 3:
+			System.out.println("Quarta-Feira");
+			break;
+		case 4:
+			System.out.println("Quinta-Feira");
+			break;
+		case 5:
+			System.out.println("Sexta-Feira");
+			break;
+		case 6:
+			System.out.println("Sábado");
+			break;
+		case 7:
+			System.out.println("Domingo");
+			break;
+
+
+		default:
+			break;
+		}
+	}
+
+
+
+
+}
