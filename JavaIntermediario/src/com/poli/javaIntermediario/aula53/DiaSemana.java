bbc863f86b43e02cc02db71c61644fa774282b95
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-12-04 15:58:01 -0300

    Enumeradores (Enum)

---
+++ JavaIntermediario/src/com/poli/javaIntermediario/aula53/DiaSemana.java
@@ -0,0 +1,7 @@
+package com.poli.javaIntermediario.aula53;
+
+public enum DiaSemana {
+	
+	SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO;
+
+}
--- JavaIntermediario/src/com/poli/javaIntermediario/aula53/Teste.java
+++ JavaIntermediario/src/com/poli/javaIntermediario/aula53/Teste.java
@@ -3,8 +3,12 @@ package com.poli.javaIntermediario.aula53;
 public class Teste {
 
 	public static void main(String[] args) {
+		
 		usandoConstantes();
+		
+		System.out.println();
 
+		usandoEnum();
 	}
 
 	private static void usandoConstantes() {
@@ -18,7 +22,7 @@ public class Teste {
 		int domingo = DiaSemanaConstantes.DOMINGO;
 
 
-		System.out.println("Teste utilizando constantes no Java");
+		System.out.println("Teste utilizando CONSTANTES no Java");
 
 		imprimeDiaSemana(segunda);	
 		imprimeDiaSemana(terca);
@@ -33,34 +37,98 @@ public class Teste {
 	private static void imprimeDiaSemana(int dia) {
 
 		switch (dia) {
+		
 		case 1:
 			System.out.println("Segunda-Feira");
 			break;
+			
 		case 2:
 			System.out.println("Terça-Feira");
 			break;
+			
 		case 3:
 			System.out.println("Quarta-Feira");
 			break;
+			
 		case 4:
 			System.out.println("Quinta-Feira");
 			break;
+			
 		case 5:
 			System.out.println("Sexta-Feira");
 			break;
+			
 		case 6:
 			System.out.println("Sábado");
 			break;
+			
 		case 7:
 			System.out.println("Domingo");
 			break;
-
-
+			
 		default:
 			break;
 		}
 	}
+	
+	private static void usandoEnum() {
+		
+		DiaSemana segunda = DiaSemana.SEGUNDA;
+		DiaSemana terca = DiaSemana.TERCA;
+		DiaSemana quarta = DiaSemana.QUARTA;
+		DiaSemana quinta = DiaSemana.QUINTA;
+		DiaSemana sexta = DiaSemana.SEXTA;
+		DiaSemana sabado = DiaSemana.SABADO;
+		DiaSemana domingo = DiaSemana.DOMINGO;
+		
+		System.out.println("Teste utilizando ENUM no Java");
+		
+		imprimeDiaSemana(segunda);	
+		imprimeDiaSemana(terca);
+		imprimeDiaSemana(quarta);
+		imprimeDiaSemana(quinta);
+		imprimeDiaSemana(sexta);
+		imprimeDiaSemana(sabado);
+		imprimeDiaSemana(domingo);
+		
+	}
+	
+	private static void imprimeDiaSemana(DiaSemana dia) {
 
+		switch (dia) {
+		case SEGUNDA:
+			System.out.println("Segunda-Feira");
+			break;
+			
+		case TERCA:
+			System.out.println("Terça-Feira");
+			break;
+			
+		case QUARTA:
+			System.out.println("Quarta-Feira");
+			break;
+			
+		case QUINTA:
+			System.out.println("Quinta-Feira");
+			break;
+			
+		case SEXTA:
+			System.out.println("Sexta-Feira");
+			break;
+			
+		case SABADO:
+			System.out.println("Sábado");
+			break;
+			
+		case DOMINGO:
+			System.out.println("Domingo");
+			break;
+
+		default:
+			break;
+			
+		}
+	}
 
 
 
