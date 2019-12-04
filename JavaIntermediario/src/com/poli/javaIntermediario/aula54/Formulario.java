4607f210e0942eeecc931b4d23a0b92ed846eed6
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-12-04 16:22:28 -0300

    Enumeradores como classe (construtor e métodos)

---
+++ JavaIntermediario/src/com/poli/javaIntermediario/aula54/Formulario.java
@@ -0,0 +1,20 @@
+package com.poli.javaIntermediario.aula54;
+
+public class Formulario {
+	
+	enum Genero{
+		
+		FEMININO('F'), MASCULINO('M');
+		
+		private char valor;
+		
+		Genero (char valor){
+			this.valor = valor;
+		}
+	}
+	
+	private String nome;
+	private Genero genero;
+	
+
+}
---
+++ JavaIntermediario/src/com/poli/javaIntermediario/aula54/TesteEnum.java
@@ -0,0 +1,15 @@
+package com.poli.javaIntermediario.aula54;
+
+public class TesteEnum {
+
+	public static void main(String[] args) {
+		
+		DiaSemana dia = DiaSemana.DOMINGO;
+		
+		System.out.println(dia.toString() + " - " + dia.getValor());
+		
+		Data data = new Data(1, 03, 2019,DiaSemana.SABADO);
+
+	}
+
+}
